ccfd289302cb3ae67159b95705335e05aec2a659
Potorac Alexandru <potorac.ioan.alexandru@gmail.com>
2022-10-17 04:26:00 +0300

    actualy finisd

--- components/ViewButton.js
+++
@@ -1,7 +0,0 @@
-import style from "../styles/Button.module.css";
-
-const ViewButton = ({ text }) => {
-    return <button className={style.buttonOut}>{text}</button>;
-};
-
-export default ViewButton;
